3be35aa48aa6c488d6cdff441b9a36a19c7766f9
N3XDROP <julian.peraltac@gmail.com>
2025-11-29 22:45:30 -0500

    feat: Configuración inicial del servidor Express.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,30 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import { AppDataSource } from "./config/db";
+import authRoutes from "./routes/auth.routes";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // URL React
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ ok: true, message: "API inicial funcionando" });
-});
+app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
+AppDataSource.initialize()
+  .then(() => {
+    console.log("✅ Base de datos conectada");
+
+    app.listen(process.env.PORT || 4000, () => {
+      console.log("✅ Servidor corriendo en puerto 4000");
+    });
+  })
+  .catch((error) => {
+    console.error("❌ Error al conectar la DB:", error);
+  });
